725d4068bfea5d2caaf6b94d5258cf87a258e60f
nina <ninajl@gmail.com>
2015-06-07 14:03:00 -0600

    reconfiguring facebook omniauth

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,7 +50,7 @@ function statusChangeCallback(response) {
 
  window.fbAsyncInit = function() {
  FB.init({
-   appId      : '{your-app-id}',
+   appId      : '{905143559567965}',
    cookie     : true,  // enable cookies to allow the server to access
                        // the session
    xfbml      : true,  // parse social plugins on this page
